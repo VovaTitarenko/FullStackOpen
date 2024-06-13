b2a1c99f393d70d17e55c89ab93128d36d085fdd
vovka from papabook <vovandarius@gmail.com>
2024-06-13 19:51:51 +0300

    configure eslint and prettier for exercises folder

--- exercises/.eslintrc.cjs
+++ exercises/.eslintrc.cjs
@@ -1,21 +1,37 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+    browser: true,
   },
-}
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'module',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    sourceType: 'module',
+  },
+  plugins: ['prettier', 'import', 'react', 'jsx-a11y'],
+  extends: ['eslint:recommended'],
+  rules: {
+    'prettier/prettier': 'warn',
+    'no-unused-vars': 'warn',
+    eqeqeq: 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
+    'no-console': 0,
+  },
+};
