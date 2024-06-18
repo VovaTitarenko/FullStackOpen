681c47e13545acad8ad62adbcc457c45c76f324a
vovka from papabook <vovandarius@gmail.com>
2024-06-18 11:33:59 +0300

    add new tests and a test DB

---
+++ notesApp-backend/tests/note_api.test.js
@@ -0,0 +1,46 @@
+const { test, after, beforeEach } = require("node:test");
+const assert = require("node:assert");
+const mongoose = require("mongoose");
+const Note = require("../models/note");
+const supertest = require("supertest");
+const app = require("../app");
+
+const initialNotes = [
+  { content: "HTML is easy", important: false },
+  { content: "Browser can execute only JavaScript", important: true },
+];
+
+const api = supertest(app);
+
+test("notes are returned as json", async () => {
+  await api
+    .get("/api/notes")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+});
+
+test("there are two notes", async () => {
+  const response = await api.get("/api/notes");
+  //   console.log(response.body);
+  assert.strictEqual(response.body.length, initialNotes.length);
+});
+
+test("the first note is about HTTP methods", async () => {
+  const response = await api.get("/api/notes");
+
+  const contents = response.body.map((e) => e.content);
+  assert(contents.includes("HTML is easy"));
+});
+
+beforeEach(async () => {
+  await Note.deleteMany({});
+  console.log("DB cleared");
+  let noteObject = new Note(initialNotes[0]);
+  await noteObject.save();
+  noteObject = new Note(initialNotes[1]);
+  await noteObject.save();
+});
+
+after(async () => {
+  await mongoose.connection.close();
+});
