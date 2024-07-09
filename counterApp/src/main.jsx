a64a32fd99d549c00ee66e3efe860d40e84a88ff
vovka from papabook <vovandarius@gmail.com>
2024-07-09 17:45:31 +0300

    part 6a: add and configure redux in a counterApp

---
+++ counterApp/src/main.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+import App from "./App.jsx";
+import "./index.css";
+import noteReducer from "./reducers/noteReducer";
+
+const store = createStore(noteReducer);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const renderApp = () => {
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  );
+};
+
+renderApp();
+store.subscribe(renderApp);
