f94768d26a50d8aeeb5db431821cf52a6353ede3
vovka from papabook <vovandarius@gmail.com>
2024-07-10 20:19:41 +0300

    refactor Redux noteApp in counterApp to Redux Toolkit

--- counterApp/src/main.jsx
+++ counterApp/src/main.jsx
@@ -1,39 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createStore, combineReducers } from "redux";
+// import { createStore, combineReducers } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 
 import App from "./App.jsx";
 import "./index.css";
+
 import noteReducer from "./reducers/noteReducer";
 import filterReducer from "./reducers/filterReducer.js";
-import { createNote } from "./reducers/noteReducer";
-import { filterChange } from "./reducers/filterReducer";
 
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer,
+// const reducer = combineReducers({
+//   notes: noteReducer,
+//   filter: filterReducer,
+// });
+
+// const store = createStore(reducer);
+
+const store = configureStore({
+  reducer: {
+    notes: noteReducer,
+    filter: filterReducer,
+  },
 });
 
-const store = createStore(reducer);
+console.log(store.getState());
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-const renderApp = () => {
-  root.render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </React.StrictMode>
-  );
-};
-
-renderApp();
-store.subscribe(renderApp);
-store.subscribe(() => console.log(store.getState()));
-
-store.dispatch(filterChange("IMPORTANT"));
-store.dispatch(
-  createNote("combineReducers forms one reducer from many simple reducers")
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
