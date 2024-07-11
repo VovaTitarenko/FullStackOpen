3718c98a405060be787d8bfeb32f912e0e3ae26f
vovka from papabook <vovandarius@gmail.com>
2024-07-11 06:54:30 +0300

    connect Redux noteApp (counterApp) to local db.json and hide async actions in reducer with Redux Thunk functional actions

--- counterApp/src/components/NewNote.jsx
+++ counterApp/src/components/NewNote.jsx
@@ -4,11 +4,11 @@ import { createNote } from "../reducers/noteReducer";
 const NewNote = () => {
   const dispatch = useDispatch();
 
-  const addNote = (event) => {
+  const addNote = async (event) => {
     event.preventDefault();
     const content = event.target.note.value;
     event.target.note.value = "";
-
+    console.log(content);
     dispatch(createNote(content));
   };
 
--- counterApp/src/main.jsx
+++ counterApp/src/main.jsx
@@ -1,28 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import { createStore, combineReducers } from "redux";
-import { configureStore } from "@reduxjs/toolkit";
+// import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
-
+import store from "./store.js";
 import App from "./App.jsx";
 import "./index.css";
 
-import noteReducer from "./reducers/noteReducer";
-import filterReducer from "./reducers/filterReducer.js";
+// import noteReducer from "./reducers/noteReducer";
+// import filterReducer from "./reducers/filterReducer.js";
 
-// const reducer = combineReducers({
-//   notes: noteReducer,
-//   filter: filterReducer,
+// const store = configureStore({
+//   reducer: {
+//     notes: noteReducer,
+//     filter: filterReducer,
+//   },
 // });
-
-// const store = createStore(reducer);
-
-const store = configureStore({
-  reducer: {
-    notes: noteReducer,
-    filter: filterReducer,
-  },
-});
 
 console.log(store.getState());
 
