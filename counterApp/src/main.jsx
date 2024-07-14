b39fa5da9fb754d8037a0755fae910c4a6c36ed5
vovka from papabook <vovandarius@gmail.com>
2024-07-14 04:13:39 +0300

    6.d create a React-Query-based noteApp

--- counterApp/src/main.jsx
+++ counterApp/src/main.jsx
@@ -1,20 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import store from "./store.js";
 import App from "./App.jsx";
 import "./index.css";
-
-// import noteReducer from "./reducers/noteReducer";
-// import filterReducer from "./reducers/filterReducer.js";
-
-// const store = configureStore({
-//   reducer: {
-//     notes: noteReducer,
-//     filter: filterReducer,
-//   },
-// });
 
 console.log(store.getState());
 
