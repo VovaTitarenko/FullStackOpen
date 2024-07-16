f9872e048014665c5ce7bd05b264f48587c5792a
vovka from papabook <vovandarius@gmail.com>
2024-07-16 09:41:12 +0300

    7.a-b use React Router and custom hooks useForm, useResource

--- counterApp/src/main.jsx
+++ counterApp/src/main.jsx
@@ -1,17 +1,22 @@
+import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 // import { Provider } from "react-redux";
 // import store from "./store.js";
 // import App from "./ReduxApp.jsx";
-import App from "./useReducerCounter";
-import "./index.css";
 
-// console.log(store.getState());
+// import App from "./useReducerCounter";
+
+// import App from "./RouterNoteApp";
+// import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    {/* <Provider store={store}> */}
-    <App />
-    {/* </Provider> */}
-  </React.StrictMode>
+    <React.StrictMode>
+        {/* <Provider store={store}> */}
+        {/* <Router> */}
+        <App />
+        {/* </Router> */}
+        {/* </Provider> */}
+    </React.StrictMode>
 );
